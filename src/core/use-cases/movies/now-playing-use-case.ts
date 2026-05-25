04019ac3cc707de4895e1264bc3e7dbfc5eb2494
Cexs010 <cesar.serafin@outlook.com>
2026-05-25 16:32:26 -0600

    chore(interfaces): rename movie-db.responses to movie-db.response and update imports

--- src/core/use-cases/movies/now-playing-use-case.ts
+++ src/core/use-cases/movies/now-playing-use-case.ts
@@ -1,11 +1,11 @@
 import { HttpAdapter } from "../../../config/adapters/http/http.adapter";
-import { NowPlayingResponse } from "../../../infrastructure/interfaces/movie-db.responses";
+import { MoviesDbResponse } from "../../../infrastructure/interfaces/movie-db.response";
 import { MovieMapper } from "../../../infrastructure/mappers/movie.mapper";
 import { type Movie } from "../../entities/movie.entity";
 
 export const moviesNowPlayingUseCase = async (fetcher: HttpAdapter): Promise<Movie[]> => {
     try {
-        const nowPlaying = await fetcher.get<NowPlayingResponse>('/now_playing')
+        const nowPlaying = await fetcher.get<MoviesDbResponse>('/now_playing')
         return nowPlaying.results.map(MovieMapper.fromMovieDBResultToEntity)
     } catch (error) {
         throw new Error('Error obtaining information from movies - now playing')
