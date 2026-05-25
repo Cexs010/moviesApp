6588e1c8fe3de30cb6d42ae00389f37795385046
Cexs010 <cesar.serafin@outlook.com>
2026-05-25 16:32:52 -0600

    feat(use-cases): implement upcoming movies use case

---
+++ src/core/use-cases/movies/upcoming-use-case.ts
@@ -0,0 +1,14 @@
+import { HttpAdapter } from "../../../config/adapters/http/http.adapter"
+import { MoviesDbResponse } from "../../../infrastructure/interfaces/movie-db.response";
+import { MovieMapper } from "../../../infrastructure/mappers/movie.mapper";
+import type { Movie } from "../../entities/movie.entity"
+
+
+export const moviesUpcoming = async (fetcher: HttpAdapter): Promise<Movie[]> => {
+    try {
+        const upcoming = await fetcher.get<MoviesDbResponse>('/upcoming');
+        return upcoming.results.map(MovieMapper.fromMovieDBResultToEntity)
+    } catch (error) {
+        throw new Error('Error obtaining information from movies - upcoming')
+    }
+}
