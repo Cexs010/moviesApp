60b59e496e99134043e9ba9a5ca451b9b5e3deaf
Cexs010 <cesar.serafin@outlook.com>
2026-05-25 16:43:55 -0600

    feat(use-cases): implement popular movies use case

---
+++ src/core/use-cases/movies/popular-use-case.ts
@@ -0,0 +1,13 @@
+import { HttpAdapter } from "../../../config/adapters/http/http.adapter"
+import { MoviesDbResponse } from "../../../infrastructure/interfaces/movie-db.response"
+import { MovieMapper } from "../../../infrastructure/mappers/movie.mapper"
+import { Movie } from "../../entities/movie.entity"
+
+export const popularMovies = async (fetcher: HttpAdapter): Promise<Movie[]> => {
+    try {
+        const popular = await fetcher.get<MoviesDbResponse>('/popular')
+        return popular.results.map(MovieMapper.fromMovieDBResultToEntity)
+    } catch (error) {
+        throw new Error('Error obtaining information from movies - popular')
+    }
+}
