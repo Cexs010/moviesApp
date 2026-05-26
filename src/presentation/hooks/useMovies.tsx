a12fa161d5bdf609c90c97727b44773b7a683485
Cexs010 <cesar.serafin@outlook.com>
2026-05-26 15:20:14 -0600

    feat(movies): add popular, top rated and upcoming movies

--- src/presentation/hooks/useMovies.tsx
+++ src/presentation/hooks/useMovies.tsx
@@ -6,19 +6,38 @@ import { movieDbFetcher } from '../../config/adapters/movieDb.adapter';
 export const useMovies = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
+  const [upComing, setUpComing] = useState<Movie[]>([]);
+  const [popular, setPopular] = useState<Movie[]>([]);
+  const [topRated, setTopRated] = useState<Movie[]>([]);
 
   useEffect(() => {
     initialLoad();
   }, []);
 
   const initialLoad = async () => {
-    const nowPlayingMovies = await UseCases.moviesNowPlayingUseCase(
-      movieDbFetcher,
-    );
+    const [
+      nowPlayingPromise,
+      upcomingPromise,
+      popularPromise,
+      topRatedPromise,
+    ] = await Promise.all([
+      UseCases.nowPlayingmoviesUseCase(movieDbFetcher),
+      UseCases.upcomingMoviesUseCase(movieDbFetcher),
+      UseCases.popularMoviesUseCase(movieDbFetcher),
+      UseCases.topRatedMoviesUseCase(movieDbFetcher),
+    ]);
+
+    setNowPlaying(nowPlayingPromise);
+    setUpComing(upcomingPromise);
+    setPopular(popularPromise);
+    setTopRated(topRatedPromise);
   };
 
   return {
     isLoading,
     nowPlaying,
+    upComing,
+    popular,
+    topRated,
   };
 };
