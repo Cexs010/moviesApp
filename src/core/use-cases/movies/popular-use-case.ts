3d9ea80c89495c47634efa45fa8b0f40d1d5ef48
Cexs010 <cesar.serafin@outlook.com>
2026-05-27 15:59:11 -0600

    fix(use-cases): fix page param type in popular movies use case

--- src/core/use-cases/movies/popular-use-case.ts
+++ src/core/use-cases/movies/popular-use-case.ts
@@ -3,9 +3,19 @@ import { MoviesDbResponse } from "../../../infrastructure/interfaces/movie-db.re
 import { MovieMapper } from "../../../infrastructure/mappers/movie.mapper"
 import { Movie } from "../../entities/movie.entity"
 
-export const popularMoviesUseCase = async (fetcher: HttpAdapter): Promise<Movie[]> => {
+interface Options {
+    page?: number;
+    limit?: number
+
+}
+
+export const popularMoviesUseCase = async (fetcher: HttpAdapter, options?: Options): Promise<Movie[]> => {
     try {
-        const popular = await fetcher.get<MoviesDbResponse>('/popular')
+        const popular = await fetcher.get<MoviesDbResponse>('/popular', {
+            params: {
+                page: options?.page ?? 1
+            }
+        })
         return popular.results.map(MovieMapper.fromMovieDBResultToEntity)
     } catch (error) {
         throw new Error('Error obtaining information from movies - popular')
