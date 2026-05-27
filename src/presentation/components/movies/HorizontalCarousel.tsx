8e76f1f02497c586c8523c9251ce7d9b5fcb5a2c
Cexs010 <cesar.serafin@outlook.com>
2026-05-27 15:59:47 -0600

    feat(components): add HorizontalCarousel component for movie lists

---
+++ src/presentation/components/movies/HorizontalCarousel.tsx
@@ -0,0 +1,58 @@
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Text,
+  View,
+} from 'react-native';
+import { Movie } from '../../../core/entities/movie.entity';
+import { globalStyles } from '../../theme/theme';
+import { MoviePoster } from './MoviePoster';
+import { useEffect, useRef } from 'react';
+
+interface Props {
+  movies: Movie[];
+  title?: string;
+  loadNextPage?: () => void;
+}
+
+export const HorizontalCarousel = ({ movies, title, loadNextPage }: Props) => {
+  const isLoading = useRef(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      isLoading.current = false;
+    }, 200);
+  }, [movies]);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isLoading.current) return;
+    const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+
+    const isEndReached =
+      contentOffset.x + layoutMeasurement.width + 600 >= contentSize.width;
+
+    if (!isEndReached) return;
+
+    isLoading.current = true;
+
+    loadNextPage && loadNextPage();
+  };
+
+  return (
+    <View style={{ height: title ? 260 : 220 }}>
+      {title && <Text style={globalStyles.title}>{title}</Text>}
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => (
+          <MoviePoster movie={item} width={140} height={200} />
+        )}
+        keyExtractor={item => item.id.toString()}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        onScroll={onScroll}
+      />
+    </View>
+  );
+};
