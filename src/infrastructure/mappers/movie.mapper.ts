160023ce93894bc523ba98354b0e9365c3860e5f
Cexs010 <cesar.serafin@outlook.com>
2026-05-28 13:11:19 -0600

    feat(movie): add full movie entity and mapper

--- src/infrastructure/mappers/movie.mapper.ts
+++ src/infrastructure/mappers/movie.mapper.ts
@@ -1,4 +1,5 @@
-import { Movie } from "../../core/entities/movie.entity";
+import { FullMovie, Movie } from "../../core/entities/movie.entity";
+import { FullMovieDBResponse } from "../interfaces/full-movie-db.response";
 import type { Result } from "../interfaces/movies-db.response";
 
 export class MovieMapper {
@@ -12,6 +13,23 @@ export class MovieMapper {
             poster: `https://image.tmdb.org/t/p/w500${result.poster_path}`,
             backdrop: `https://image.tmdb.org/t/p/w500${result.backdrop_path}`,
 
+        }
+    }
+
+    static fromFullMovieDBResultToEntity(movie: FullMovieDBResponse): FullMovie {
+        return {
+            id: movie.id,
+            title: movie.title,
+            description: movie.overview,
+            realeseDate: new Date(movie.release_date),
+            rating: movie.vote_average,
+            poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+            genres: movie.genres.map(genre => genre.name),
+            duration: movie.runtime,
+            budget: movie.budget,
+            originalTitle: movie.original_title,
+            productionCompanies: movie.production_companies.map(company => company.name),
         }
     }
 }
