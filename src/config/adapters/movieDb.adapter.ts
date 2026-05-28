bbf25c638fd2ac194162dd5a9f5aefdbf257f77d
Cexs010 <cesar.serafin@outlook.com>
2026-05-28 17:04:41 -0600

    chore(env): configure dotenv support for api keys

--- src/config/adapters/movieDb.adapter.ts
+++ src/config/adapters/movieDb.adapter.ts
@@ -1,9 +1,10 @@
+import { MOVIE_DB_API_KEY } from "@env";
 import { AxiosAdapter } from "./http/axios.adapter";
 
 export const movieDbFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: '2995e8cab62b3b44ab6e79e2c710623f',
+        api_key: MOVIE_DB_API_KEY,
         language: 'es'
     }
 })
